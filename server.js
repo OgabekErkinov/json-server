3407a776e5ffeb9b6025d7d597e27de1b2981f71
Ogabek Erkinov <ogabekerkinov56@gmail.com>
2025-03-29 06:21:14 +0500

    Fixed server.js and package.json for Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-const PORT = 5000; // JSON Server porti
+const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log(`✅ JSON Server is running on port ${PORT}`);
 });
